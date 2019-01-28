0758d873b247d2644e305dbc254582c0b2641337
Vyshag.V <Codeplacer@outlook.com>
2019-01-28 19:35:26 +0530

    Adding Algorithm master project with linear search function

---
+++ Algorithms/LinearSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class LinearSearch
+    {
+        public static int search(int[] inputArray, int elementToSearch)
+        {
+            int i;
+            for (i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] == elementToSearch)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINEAR SEARCH
+            int[] arr = { 1, 10, 30, 15 };
+            int x = 30;
+            Console.WriteLine("Linear Search Algorithm : " + x + " is present at index " + LinearSearch.search(arr, x));
+            Console.Read();
+        }
+    }
+}
