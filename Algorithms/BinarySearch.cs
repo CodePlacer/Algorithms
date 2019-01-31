9c9365f78ffbca65865f376cc4d78f005eb85f62
Vyshag.V <Codeplacer@outlook.com>
2019-01-31 15:13:34 +0530

    Added Binary search

---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class BinarySearch
+    {
+        static List<int> output = new List<int>();
+        public static string binarySearch(int[] inputArray, int index, int arrayElementCount, int elementToSearch)
+        {
+            if (arrayElementCount >= index)
+            {
+                int mid = index + (arrayElementCount - index) / 2;
+
+                //Checking the array index value with input
+                if (inputArray[mid] == elementToSearch)
+                {
+                    output.AddRange(inputArray.Skip(mid + 1));
+                    return string.Join(",", output);
+                }
+
+                // If element is smaller than mid, then 
+                // it can only be present in left subarray 
+                if (inputArray[mid] > elementToSearch)
+                    return binarySearch(inputArray, index, mid - 1, elementToSearch);
+
+                // Else the element can only be present 
+                // in right subarray 
+                return binarySearch(inputArray, mid + 1, arrayElementCount, elementToSearch);
+            }
+
+            // if we reach here, then element was 
+            // not present 
+            return string.Join(",", output);
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,11 +10,20 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            //LINEAR SEARCH
+
             int[] arr = { 1, 10, 30, 15, 23, 44, 62, 1, 4, 8, 6, -6 };
             int x = 30;
-            Console.WriteLine("Given Number : " + x );
-            Console.WriteLine("Linear Search Algorithm Result : " + LinearSearch.search(arr, x) + " is/are greater than " + x);
+            Console.WriteLine("Given Number : " + x);
+
+
+            //LINEAR SEARCH
+            Console.WriteLine("Linear Search Algorithm Result : " + LinearSearch.search(arr, x) + " are greater than " + x);
+
+
+            //BINARY SEARCH
+            Console.WriteLine("Binary Search Algorithm Result : " + BinarySearch.binarySearch(arr.OrderBy(y => y).ToArray(), 0, arr.Length - 1, x) + " are greater than " + x);
+
+
             Console.Read();
         }
     }
